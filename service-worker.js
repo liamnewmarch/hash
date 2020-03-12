2a8a09f3af924925561b2043d8fe3623b72717e9
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2020-03-12 20:41:38 +0000

    Add images to service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,20 @@
-const CACHE_VERSION = 2;
+const CACHE_VERSION = 3;
 
 const CACHE_URLS = [
-  '',
+  '.',
   'app.css',
   'app.js',
+  'images/icon-48.png',
+  'images/icon-72.png',
+  'images/icon-96.png',
+  'images/icon-144.png',
+  'images/icon-192.png',
+  'images/icon-512.png',
+  'index.html',
 ];
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(async () => {
+const CACHE_STRATEGY = {
+  async activate() {
     const cacheVersions = await caches.keys();
     for (const cacheVersion of cacheVersions) {
       if (cacheVersion !== CACHE_VERSION) {
@@ -16,11 +23,16 @@ self.addEventListener('activate', (event) => {
     }
     const cache = await caches.open(CACHE_VERSION);
     return cache.addAll(CACHE_URLS);
-  });
+  },
+  async fetch(event) {
+    return await caches.match(event.request) || await fetch(event.request);
+  },
+};
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(CACHE_STRATEGY.activate(event));
 });
 
 self.addEventListener('fetch', (event) => {
-  event.waitUntil(async (event) => {
-    return await caches.match(event.request) || await fetch(event.request);
-  });
+  event.waitUntil(CACHE_STRATEGY.fetch(event));
 });
